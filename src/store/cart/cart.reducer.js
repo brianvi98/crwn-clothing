58ac1ade214c49961d781fcca619738cae8635bf
brianvi98 <brianvi98@gmail.com>
2025-09-20 23:27:35 -0700

    final commit

--- src/store/cart/cart.reducer.js
+++ src/store/cart/cart.reducer.js
@@ -35,8 +35,9 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
   );
 };
 
-const clearCartItem = (cartItems, cartItemToClear) =>
-  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+const clearCartItem = (cartItems, cartItemToClear) => {
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+};
 
 const CART_INITIAL_STATE = {
   isCartOpen: false,
--- src/store/store.js
+++ src/store/store.js
@@ -1,36 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { compose, createStore, applyMiddleware } from "redux";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
+import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-// const composeEnhancer =
-//   (process.env.NODE_ENV !== "production" &&
-//     window &&
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-//   compose;
-
-// const persistConfig = {
-//   key: "root",
-//   storage,
-//   blacklist: ["user"],
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // default middlewares: redux-thunk, serializability checks, immutability checks
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(middleWares),
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(process.env.NODE_ENV === "development" ? [logger] : []),
+  devTools: process.env.NODE_ENV !== "production",
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
